6e06acb9612b6bb1e0152d65f1985d9b319d5a96
kritpong t. <krittapongtip@gmail.com>
2026-08-04 23:37:27 +0700

    hide home page for now

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -17,25 +17,26 @@ final class HomeController
 
 	public function index(Request $request): Response
 	{
-		return Response::html($this->view->renderPage('home/index', [
-			'env' => $this->config->get('app.env')
-		]));
+		// return Response::html($this->view->renderPage('home/index', [
+		// 	'env' => $this->config->get('app.env')
+		// ]));
+		return Response::html('<h1>Hello World</h1>');
 	}
 
-	public function greet(Request $request): Response
-	{
-		$name = trim((string) $request->input('name', ''));
+	// public function greet(Request $request): Response
+	// {
+	// 	$name = trim((string) $request->input('name', ''));
 
-		// htmx call -> fragment only, htmx swaps it into #greeting
-		if($request->isHtmx()){
-			return Response::html($this->view->renderPartial('greeting', [
-				'name' => $name
-			]));
-		}
+	// 	// htmx call -> fragment only, htmx swaps it into #greeting
+	// 	if($request->isHtmx()){
+	// 		return Response::html($this->view->renderPartial('greeting', [
+	// 			'name' => $name
+	// 		]));
+	// 	}
 
-		return Response::html($this->view->renderPage('home/index', [
-			'env' => $this->config->get('app.env'),
-			'name' => $name
-		]));
-	}
+	// 	return Response::html($this->view->renderPage('home/index', [
+	// 		'env' => $this->config->get('app.env'),
+	// 		'name' => $name
+	// 	]));
+	// }
 }
